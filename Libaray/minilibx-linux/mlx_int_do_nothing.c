45951517b51b3d73870cecefbbf1a8c9adaf8b96
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-05-06 13:55:43 +0930

    start over

--- Libaray/minilibx-linux/mlx_int_do_nothing.c
+++
@@ -1,5 +0,0 @@
-
-int	mlx_int_do_nothing(void *param)
-{
-  
-}
--- srcs/common/quit_app.c
+++
@@ -1,8 +0,0 @@
-#include "../../main.h"
-
-void	quit_app(t_env *env, const char *message, int code)
-{
-	printf("Error\n%s\n", message);
-	free_env(env);
-	exit(code);
-}
